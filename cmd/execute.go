79bc61643547a8d0e25205f932d193f3de826a7b
sushimig <mochidukikeisansuki@gmail.com>
2025-01-16 20:02:47 +0900

    add json convert function

--- cmd/execute.go
+++ cmd/execute.go
@@ -9,8 +9,9 @@ import (
 func Execute() {
 	dir := flag.String("dir", ".", "specify the directory name you want.")
 	exts := flag.String("ext", "", "specify extentions you want. Separate by slash.")
+	isJson := flag.Bool("json", false, "select ture or false if you want to convert to json.")
 	flag.Parse()
 
 	extList := strings.Split(*exts, "/")
-	display.Display(*dir, extList)
+	display.Display(*dir, extList, *isJson)
 }
